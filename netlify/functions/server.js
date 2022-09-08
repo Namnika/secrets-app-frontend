779f029f057886feee89837c97bf5ce7a783d600
Namnika <janbandhunamnika@gmail.com>
2022-09-09 00:14:07 +0530

    Update files

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -5,3 +5,16 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => app.listen(port, () => console.log("Server is running on port 5000")))
+.catch((err) => console.log(err.message));
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
