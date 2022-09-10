bba5e7fa8e06b1bc382c203f1e9895435a2c6398
Namnika <janbandhunamnika@gmail.com>
2022-09-10 21:52:14 +0530

    Update files

---
+++ netlify/functions/routes/users.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const bodyParser = require("body-parser");
+let User = require("../models/user.model.js");
+
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.route("/").get((req, res) => {
+  User.find()
+  .then(users = res.json(users))
+  .catch(err => res.status(400).json("Error: " + err))
+  
+});
+
+router.route("/login").post((req, res) => {
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+  newUser.save();
+  .then(()=>res.send("User added!"))
+  .catch(err => res.status(400).json("Error: " + err))
+});
+
+module.exports = router;
--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -20,7 +20,4 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 
 const usersRouter = require("./routes/users");
-app.use("/register", usersRouter);
-app.use("/login", usersRouter);
-app.use("/secrets", usersRouter);
-app.use("/submit", usersRouter);
+app.use("/", usersRouter);
