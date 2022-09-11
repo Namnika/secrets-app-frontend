2a8be143ed1fd8ffdab06bcf1790930e4b2e4c44
Namnika <janbandhunamnika@gmail.com>
2022-09-11 21:33:39 +0530

    Update files

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -6,18 +6,22 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+
+
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => app.listen(port, () => console.log("Server is running on port 5000")))
 .catch((err) => console.log(err.message));
+
 
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.json());
+
 
 const usersRouter = require("./routes/users");
 app.use("/register", usersRouter);
