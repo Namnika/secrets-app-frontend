801b57a597d40883a28d52b2231ef70387a84435
Namnika <janbandhunamnika@gmail.com>
2022-09-21 00:15:19 +0530

    Update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from "./login";
 import Home from './home';
 import Register from "./register";
