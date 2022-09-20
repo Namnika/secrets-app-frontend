3f841170e80a6b0e6b945e6b481b00dee3634a11
Namnika <janbandhunamnika@gmail.com>
2022-09-20 19:26:03 +0530

    Update files

--- netlify/functions/routes/auth.js
+++ netlify/functions/routes/auth.js
@@ -1,3 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const User = require("../models/user.model");
+
+router.get("/", (req, res) => {
+  res.render("home");
+});
+
+
+router.post("/register", (req, res, next) => {
+  passport.authenticate("local", function(err, user, info){
+    if (err) {
+      return res.status(400).json({ errors: err });
+    }
+    if (!user) {
+      return res.status(400).json({ errors: "No user found" });
+    }
+    res.redirect("/secrets");
+  })(req, res, next);
+});
+
+router.post("/login", (req, res, next) => {
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  passport.authenticate("local", function(err, user, info){
+    if (err) {
+      return res.status(400).json({ errors: err });
+    }
+    if (!user) {
+      return res.status(400).json({ errors: "No User Exists" });
+    }
+    req.logIn(user, function(err){
+      if (err) {
+        return res.status(400).json({ errors: err });
+      }
+      res.send("successfully Logged In");
+      res.redirect("/secrets");
+      console.log(req.user);
+    })
+  })
+})
