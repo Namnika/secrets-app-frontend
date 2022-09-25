1928cf67d8ae28206dbdd0f87d379c9e65c21581
Namnika <janbandhunamnika@gmail.com>
2022-09-25 17:04:34 +0530

    Update files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node-modules/,
+      test: /\.js$/,
+      loader: 'babel-loader'
+    }]
+  },
+  
+}
