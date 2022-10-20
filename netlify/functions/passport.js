aae01da75ea06fc5e1adccf1d484ac5a9f1836aa
Namnika <janbandhunamnika@gmail.com>
2022-10-20 22:42:15 +0530

    Update files

--- netlify/functions/passport.js
+++ netlify/functions/passport.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://auth-blond.vercel.app/auth/google/callback",
+      callbackURL: "https://auth-blond.vercel.app/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, cb) {
       User.findOrCreate(
