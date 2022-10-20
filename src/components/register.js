8cd31d31183353ca9b17f26bfb0222a24df563de
Namnika <janbandhunamnika@gmail.com>
2022-10-20 18:23:02 +0530

    Update files

--- src/components/register.js
+++ src/components/register.js
@@ -19,11 +19,11 @@ const REGISTER_URL = "/register";
 
 function Register() {
   const google = () => {
-    window.open("https://auth-blond.vercel.app/auth/google", "_self");
+    window.open("https://secret-app.netlify.app/auth/google", "_self");
   };
 
   const github = () => {
-    window.open("https://auth-blond.vercel.app/auth/github", "_self");
+    window.open("https://secret-app.netlify.app/auth/github", "_self");
   };
 
   const facebook = () => {
