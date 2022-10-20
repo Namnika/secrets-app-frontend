77b3d19c36f83452d64616d70ea11710f15b9cd6
Namnika <janbandhunamnika@gmail.com>
2022-10-20 23:02:43 +0530

    Update files

--- netlify/functions/config/allowedOrigins.js
+++ netlify/functions/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
    'https://www.yoursite.com',
    'http://127.0.0.1:5500',
    'https://auth-blond.vercel.app',
-   'https://secret-app.netlify.app'
+   'https://secret-app.netlify.app',
+   'http://localhost:5000'
 ];
 
 module.exports = allowedOrigins;
