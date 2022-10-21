a5e128023daac24cbd392f1296a68b817367c4fb
Namnika <janbandhunamnika@gmail.com>
2022-10-21 11:01:36 +0530

    Update files

--- netlify/functions/routes/secrets.js
+++ netlify/functions/routes/secrets.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const secretsController = require('../controllers/secretsController');
 
-router.get('/', secretsController.handleSecret);
+router.post('/', secretsController.handleSecret);
 
 module.exports = router;
--- src/components/register.js
+++ src/components/register.js
@@ -144,7 +144,7 @@ function Register() {
   return (
     <>
       {success ? (
-        navigate("https://secret-app.netlify.app/secrets")
+        navigate("/secrets")
       ) : (
         <div className="container mt-5 home">
           <h1>Sign Up</h1>
