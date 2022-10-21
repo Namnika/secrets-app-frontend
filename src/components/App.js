246e9318f5cc9b3840ad0efc0a0fa7f85058cfc1
Namnika <janbandhunamnika@gmail.com>
2022-10-21 13:05:39 +0530

    update files

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./home";
 import Login from "./login";
 import Register from "./register";
