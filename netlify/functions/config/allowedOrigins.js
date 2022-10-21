bf49b1d6c6722b8d913b87d52a574ece52deae9c
Namnika <janbandhunamnika@gmail.com>
2022-10-21 18:02:57 +0530

    Update files

--- netlify/functions/config/allowedOrigins.js
+++
@@ -1,5 +0,0 @@
-const allowedOrigins = [
-   'https://secretapp.onrender.com'
-];
-
-module.exports = allowedOrigins;
--- netlify/functions/routes/submit.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const submitController = require('../controllers/submitController');
-
-router.post('/', submitController.handleSubmit);
-
-module.exports = router;
