a422b292c814c17b15fa53100e9b82e6ad87361c
Namnika <janbandhunamnika@gmail.com>
2022-10-22 20:38:52 +0530

    Update files

--- functions/config/allowedOrigins.js
+++ functions/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
    'https://secret-app.stormkit.dev',
-   'https://secret-api.vercel.app'
+   'https://secret-api.vercel.app',
+   'http://localhost:5000'
 ];
 
 module.exports = allowedOrigins;
