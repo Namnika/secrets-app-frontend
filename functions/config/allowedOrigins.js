6fcc2b8806857e28dbb380f754c8cea96f529b15
Namnika <janbandhunamnika@gmail.com>
2022-10-24 10:02:08 +0530

    Update files

--- functions/config/allowedOrigins.js
+++ functions/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
    'https://secrets-app-five.vercel.app',
-   'https://secrets-app-frontend.vercel.app'
+   'https://secrets-app-frontend.vercel.app',
+   'http://localhost:5000',
+   'http://localhost:3000'
 ];
 
 module.exports = allowedOrigins;
